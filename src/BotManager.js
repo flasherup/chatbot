462c33bde183c4ffc46803403e27cf0e2534afd9
Valeriy Vostrikov <>
2020-01-20 08:14:19 +0200

    Add Bot manager instead of process-event

---
+++ src/BotManager.js
@@ -0,0 +1,63 @@
+'use strict';
+const store = require('./reminder/RemindersStore')({ path: process.cwd() + '/reminders.json' });
+const sendMessage = require('./send-message')
+const Bot = require('./reminder/Bot')
+const dialogFlow = require('./reminder/DialogFlow')();
+
+class BotManager {
+    constructor(updateInterval) {
+        this.updateInterval = updateInterval;
+        this.bots = this._restoreBots(store, dialogFlow);
+        this.checkIntervalId = this.startRemindersCheck(this.updateInterval);
+        this._checkReminders();
+    }
+
+    //Process message event with from chat
+    //and send reply if needed
+    //Create new bot in case of new user
+    processMessengerEvent(event) {
+        const userId = event.sender.id;
+        if (!this.bots[userId]) this.bots[userId] = new Bot(userId, store, dialogFlow)
+        
+        this.bots[userId].processEvent(event).then(resp => {
+            sendMessage(resp);
+        }).catch( err => {
+            console.log('can not process event', err)
+        });
+    }
+
+    startRemindersCheck(interval) {
+        return setInterval(() => { this._checkReminders() }, interval);
+    }
+
+    stopRemindersCheck() {
+        clearInterval(this.checkIntervalId)
+    }
+
+    //Restore all reminders saved in store
+    _restoreBots(store, dialogFlow) {
+        let bots = {};
+        const keys = store.data;
+        for (let key in keys) {
+            console.log(key);
+            bots[key] = new Bot(key, store, dialogFlow)
+        }
+        return bots;
+    }
+
+    //Check if any reminder ready for alert
+    //And if it is ready, than fire reminder
+    _checkReminders() {
+        console.log('checkReminders');
+        let reminders = [];
+        for (let key in this.bots) {
+            reminders = this.bots[key].getReadyReminders();
+            console.log('bot', key, 'reminders', reminders);
+            reminders.forEach(r => {
+                sendMessage(r);
+            });
+        }
+    }
+}
+
+module.exports = BotManager;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 require('dotenv').config({ path: 'variables.env' })
 const express = require('express');
 const bodyParser = require('body-parser');
+const BotManager = require('./BotManager')
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,5 +12,17 @@ app.listen(5000, () => console.log('Express server is listening on port 5000'));
 const verifyWebhook = require('./verify-webhook');
 app.get('/', verifyWebhook);
 
-const messageWebhook = require('./message-webhook');
-app.post('/', messageWebhook);
+const updateInterval = 30 * 1000;
+const botManager = new BotManager(updateInterval)
+
+app.post('/', (req, res) => {
+    if (req.body.object === 'page') {
+        req.body.entry.forEach(entry => {
+            entry.messaging.forEach(event => {
+                botManager.processMessengerEvent(event);
+            });
+        });
+
+        res.status(200).end();
+    }
+});
